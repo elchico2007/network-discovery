5e606db849ca2c1a6550a4591191535dbd5c0d2c
elchico2007 <umblander06@gmail.com>
2020-10-28 19:11:09 -0400

    Adding comments, setting root_device

--- lldp_graph.py
+++ lldp_graph.py
@@ -1,51 +1,58 @@
 #!/usr/bin/python3
-
 from napalm import get_network_driver
 import json
 
+# importing driver for cisco
 driver = get_network_driver('ios')
 
-#devices = ['192.168.1.1', '192.168.1.2']
+# specifying root device from which we will begin to search
+root_device = 'cisco1'
 
-device = 'cisco1'
-
+# setting lldp devices to an empty list
 lldp_devices = []
 
-def get_lldp_neigh(device):
+def get_lldp_neighbors(device):
 
     local_interfaces = []
     local_neighbors = []
     lldp_device = {}
 
+    # specifying authentication vars
     username = 'admin'
     password = 'admin'
     optional = {'secret': 'admin'}
 
+    # establishing connection parameters
     device_con = driver(device, username, password, optional_args=optional )
 
+    # opening SSH connection
     device_con.open()
 
+    # using napalm to collect lldp neighhbors
     device_facts = device_con.get_lldp_neighbors()
 
+    # close SSH connection
     device_con.close()
 
+    # removing '.testing' domain name from all lldp neighbors collected
     for entry in device_facts:
         for neighbor in device_facts[entry]:
             local_neighbors.append(neighbor['hostname'].replace('.testing', ''))
             local_interfaces.append(entry)
     
+    # setting up dict keys
     lldp_device['host'] = device
     lldp_device['neighbors_hostname'] = local_neighbors
     lldp_device['interfaces'] = local_interfaces
 
-    #print(lldp_devices)
-
+    # if device is not already in the list add it
     if device not in lldp_devices:
         lldp_devices.append(lldp_device)
 
+    # return dictionary
     return lldp_device
 
-lldp_devices = [ get_lldp_neigh(device) ]
+lldp_devices = [ get_lldp_neighbors(root_device) ]
 
 def lldp_discovery():
 
@@ -63,21 +70,8 @@ def lldp_discovery():
                 lldp_devices_needing_query.append(device)
 
     for device in lldp_devices_needing_query:
-        for current_device in range(len(lldp_devices)):
-            for index, entry in enumerate(lldp_devices[current_device]['neighbors_hostname']):
-                if device != entry:
-                    new_dev_to_query = lldp_devices[current_device]['neighbors_hostname'][index]
-                    get_lldp_neigh(new_dev_to_query)
-                else:
-                    pass
-    all_devices = []
-
-    for device in lldp_devices:
-        if device not in all_devices:
-            all_devices.append(device)
-
-    return all_devices
+        get_lldp_neighbors(device)
 
 lldp_discovery()
 
-print(lldp_devices)
+print(json.dumps(lldp_devices, indent=2))
