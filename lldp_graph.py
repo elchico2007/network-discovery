ba3c217d67b16b6119af0fcb2e87b8689169699d
elchico2007 <umblander06@gmail.com>
2020-10-28 19:44:54 -0400

    Adding func to discover neighbors which are not directly connected

--- lldp_graph.py
+++ lldp_graph.py
@@ -6,7 +6,7 @@ import json
 driver = get_network_driver('ios')
 
 # specifying root device from which we will begin to search
-root_device = 'cisco1'
+root_device = 'cisco4'
 
 # setting lldp devices to an empty list
 lldp_devices = []
@@ -52,6 +52,7 @@ def get_lldp_neighbors(device):
     # return dictionary
     return lldp_device
 
+# converting each entry to an item in a list
 lldp_devices = [ get_lldp_neighbors(root_device) ]
 
 def lldp_discovery():
@@ -72,6 +73,31 @@ def lldp_discovery():
     for device in lldp_devices_needing_query:
         get_lldp_neighbors(device)
 
+def extend_neighbor_discovery(lldp_devices):
+    all_neighbors = []
+    all_discovered_neighbors = []
+
+    for device in lldp_devices:
+        all_neighbors.append(device['neighbors_hostname'])
+
+    for discovered_device in lldp_devices:
+        all_discovered_neighbors.append(discovered_device['host'])
+    
+    # flatten list to be a single list
+    all_neighbors = [ item for sublist in all_neighbors for item in sublist ]
+
+    # removing duplicates
+    all_neighbors = list(set(all_neighbors))
+    
+    # executing get_lldp_neighbors function if there are undiscovered neighbors
+    for device in all_neighbors:
+        if device not in all_discovered_neighbors:
+            get_lldp_neighbors(device)
+
+# execute lldp_discovery
 lldp_discovery()
+
+# execute extended function to find not directly connected neighbors
+extend_neighbor_discovery(lldp_devices)
 
 print(json.dumps(lldp_devices, indent=2))
